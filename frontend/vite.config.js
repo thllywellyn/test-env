327849fd04eef8a0c1157446ae2348b3b417a3c9
Llywellyn Sana <71801906+thllywellyn@users.noreply.github.com>
2025-03-04 14:06:54 +0000

    xdtcvgujokpl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://test-env-0xqt.onrender.com', // Change this to your backend URL
+      "/api": {
+        target: "https://test-env-0xqt.onrender.com",
         changeOrigin: true,
         secure: false,
       },
