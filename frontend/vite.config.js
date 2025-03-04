fc1275f52cf0f0fcc31d740b08281af71d46ae19
Llywellyn Sana <71801906+thllywellyn@users.noreply.github.com>
2025-03-04 17:07:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:4400'
+      '/api': 'https://test-env-0xqt.onrender.com'
     }
   },
   plugins: [react()],
