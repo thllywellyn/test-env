096eaee33117ca4ef13d7c52318e82e426a27588
Llywellyn Sana <71801906+thllywellyn@users.noreply.github.com>
2025-03-04 14:51:33 +0000

    sdfas

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,13 @@ import Razorpay from "razorpay"
 
 const app = express();
 
-app.use(cors())
+const corsOptions = {
+    origin: ["*","https://test-env-kappa.vercel.app", "https://*.lsanalab.xyz"], // specify allowed origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // specify allowed methods
+    credentials: true // allow cookies to be sent
+};
+
+app.use(cors(corsOptions))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
