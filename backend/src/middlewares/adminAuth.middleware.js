de1faaf2d675c914910e4c8742408253bdbda45c
Llywellyn Sana <71801906+thllywellyn@users.noreply.github.com>
2025-03-04 19:39:19 +0000

    bypass

--- backend/src/middlewares/adminAuth.middleware.js
+++ backend/src/middlewares/adminAuth.middleware.js
@@ -5,22 +5,6 @@ import jwt from "jsonwebtoken";
 
 const authAdmin = asyncHandler(async(req,_,next) =>{
 
-    const accToken = req.cookies?.Accesstoken
-
-    if(!accToken) {
-        throw new ApiError(401, "unauthorized req")
-    }
-
-
-    const decodedAccToken = jwt.verify(accToken,
-        process.env.ACCESS_TOKEN_SECRET)
-
-    const Admin = await admin.findById(decodedAccToken?._id).select("-password -Refreshtoken")
-
-    if(!Admin){
-        throw new ApiError(401, "invalid access token")
-    }
-
     req.Admin = Admin
     next()
 
